f97a630b964ef6b4cb3f8ccee22706bcdc970dec
solarsema <solyeshitla10@gmail.com>
2024-07-21 15:25:25 -0400

    intial commit

---
+++ nexflix-clone-2024/src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+const Header = () => {
+  return (
+           < div className='header_outer_container'>
+              <div div className='header_container'>
+                 <div className=' header_left'>
+              <ul>
+              <li>Netfilex</li>
+              <li>Home</li>
+              <li>TvShows</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>MyList</li>
+              <li>Brown by Languages</li>
+             </ul>
+            </div>
+            </div>
+            </div>
+  )
+}
+
+export default Header
