809676aafaa0603dde74f8012299dfa9353853ae
solarsema <solyeshitla10@gmail.com>
2024-07-21 20:09:24 -0400

    header and footer

--- nexflix-clone-2024/src/Components/Footer/Footer.js
+++ nexflix-clone-2024/src/Components/Footer/Footer.js
@@ -1,9 +1,61 @@
 import React from 'react'
-
-function Footer() {
+// import FacebookIcon from '@mui/icons-material/Facebook';
+// import InstagramIcon from '@mui/icons-material/Instagram';
+// import YouTubeIcon from '@mui/icons-material/YouTube';
+import "./Footer.css"
+const Footer=() =>{
   return (
-    <div>Footer</div>
+    <div className='footer_outer_contener'>
+      <div className='footer_inner_contener'>
+             <div className='footer_icons'>
+                 {/* <FacebookOutlinedIcon/>
+                 <InstagramIcon/>
+                 <YouTubeIcon/> */}
+              </div>
+              <div className='footer_date'>
+                <div>
+                   <ul>
+                      <li>Audio Descripion</li>
+                      <li>Investor Relations</li>
+                      <li> Legal Notice</li>
+                   </ul>
+                </div>
+                <div>
+                  <ul>
+                    <li>Help Center</li>
+                    <li>Jobs</li>
+                    <li>Cookie Preferences</li>
+                  </ul>
+                </div>
+                <div>
+                  <ul>
+                    <li>Gift Card</li>
+                    <li>Term of Use</li>
+                    <li>Corporate Information</li>
+                  </ul>    
+                </div>
+                <div>
+                  <ul>
+                    <li>Medical Center</li>
+                    <li>Privacy</li>
+                    <li>Contact Us</li>
+                  </ul>
+                </div>
+              </div>
+               <div className='Service_Code'>
+                   <p>
+                     Service Code
+                   </p>
+               </div>
+               <div className='Copy_Write'>
+                    &copy; 1997-2024 Netflix,Inc.
+               </div>
+       </div>
+    </div>
+    
+    
   )
 }
+
 
 export default Footer
--- nexflix-clone-2024/src/Components/Header/Header.js
+++ nexflix-clone-2024/src/Components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./Header.css";
-import SearchIcon from '@mui/icons-material/Search';
+import Netflixlogo from "../../Assets/Images/th.jpeg"
+// import SearchIcon from '@mui/icons-material/Search';
+// import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+// import AccountBoxIcon from '@mui/icons-material/AccountBox';
+// import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
 const Header = () => {
   return (
-           < div className='header_outer_container'>
-              <div div className='header_container'>
-                 <div className=' header_left'>
-              <ul>
+   < div className='header_outer_container'>
+      <div div className='header_container'>
+         <div className='header_left'>
+            <ul>
+               <li>  </li>
               <li>Netfilex</li>
               <li>Home</li>
               <li>TvShows</li>
@@ -14,10 +19,18 @@ const Header = () => {
               <li>Latest</li>
               <li>MyList</li>
               <li>Brown by Languages</li>
-             </ul>
-            </div>
-            </div>
-            </div>
+            </ul>
+         </div>
+         {/* <div className='header_right'>
+            <ul>
+              <li><SearchIcon/></li> 
+              <li><NotificationNoneIcon/></li>
+              <li><AccountBoxIcon/></li>
+              <li><ArrowDropDownIcon/></li>
+            </ul>
+         </div> */}
+     </div>
+   </div>
   )
 }
 
